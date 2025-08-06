75ee370d0652ea4436d798fb0b96ab4c5be4d4b5
Lynzerne <lyndsayg@gmail.com>
2025-08-06 10:28:04 -0600

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -390,8 +390,7 @@ for _, row in merged.iterrows():
         fg_all.add_to(m)
         fg_diversion.add_to(m)
         folium.LayerControl(collapsed=True).add_to(m)
-        
-return m
+        return m
 
 # --- Plotly chart function for selected station ---
 
