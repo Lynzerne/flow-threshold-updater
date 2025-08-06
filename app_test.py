4c280bee8c60534d1924f15001db0775e489299d
Lynzerne <lyndsayg@gmail.com>
2025-08-06 11:48:29 -0600

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -357,7 +357,6 @@ def render_map_clickable(merged, selected_dates):
         compliance_color = get_color_for_date(row, date)
 
         border_color = 'blue' if wsc in diversion_tables else 'black'
-        tooltip_text = f"{wsc} | {station_name}"
         # Marker with tooltip only (station code) — NO popup here
         marker = folium.CircleMarker(
             location=coords,
@@ -367,7 +366,7 @@ def render_map_clickable(merged, selected_dates):
             fill=True,
             fill_color=compliance_color,
             fill_opacity=0.7,
-            tooltip=tooltip_text
+            tooltip= f"{wsc} | {station_name}"
         )
         marker.add_to(fg_all)
 
@@ -380,7 +379,7 @@ def render_map_clickable(merged, selected_dates):
                 fill=True,
                 fill_color=compliance_color,
                 fill_opacity=0.7,
-                tooltip=tooltip_text
+                tooltip= f"{wsc} | {station_name}"
             )
             marker2.add_to(fg_diversion)
 
